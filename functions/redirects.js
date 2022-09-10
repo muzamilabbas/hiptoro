3eae4a9f080da96675ab0b9fe2d8f5726944f8a9
Muzamil Abbas <muzamilabbas2010@yahoo.com>
2022-09-11 00:41:23 +0500

    function

--- functions/redirects.js
+++ functions/redirects.js
@@ -1,10 +1,10 @@
 
 exports.handler = async event => {
-  if (event.headers.referer.includes('facebook')) {
+  if (event.headers.referer && event.headers.referer.includes('facebook')) {
     return {
       statusCode: 301,
       headers: {
-        location: decodeURIComponent(event.queryStringParameters.url.replace(/\+/g, '%20'))
+        location: `https://www.hiptoro.com/?page_id=${event.headers.referer.pid}` 
       }
     }
   } else {
@@ -19,3 +19,4 @@ exports.handler = async event => {
 }
 
 // https://wondrous-malasada-3ffc32.netlify.app/.netlify/functions/post/1001/
+// https://wondrous-malasada-3ffc32.netlify.app/.netlify/functions/redirects?pid=20955
