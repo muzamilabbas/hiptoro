6767bb6147d709074eac9bb66e14a539c37fe7b9
Muzamil Abbas <muzamilabbas2010@yahoo.com>
2022-09-10 18:05:17 +0500

    function

---
+++ functions/redirects.js
@@ -0,0 +1,20 @@
+exports.handler = async event => {
+  if (event.headers.referer.includes('facebook')) {
+    return {
+      statusCode: 301,
+      headers: {
+        location: decodeURIComponent(event.queryStringParameters.url.replace(/\+/g, '%20'))
+      }
+    }
+  } else {
+    let pathName = location.pathname.split('/')[2].split('-')
+    return {
+      statusCode: 301,
+      headers: {
+        location: process.env.URL + pathName[0] + '/' + pathName[1]
+      }
+    }
+  }
+}
+
+// https://wondrous-malasada-3ffc32.netlify.app/.netlify/functions/post/1001/
