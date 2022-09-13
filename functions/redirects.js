8e933dbc57c8bf8bef65babd3afa3f052ee16c19
muzamilabbas <muzamilabbas2010@yahoo.com>
2022-09-13 23:58:24 +0500

    Update redirects.js

--- functions/redirects.js
+++ functions/redirects.js
@@ -1,21 +1,24 @@
-
 exports.handler = async event => {
-  if (event.headers.referer && event.headers.referer.includes('facebook')) {
-    return {
-      statusCode: 301,
-      headers: {
-        location: `https://www.hiptoro.com/?page_id=${event.queryStringParameters.pid}` 
-      }
-    }
-  } else {
-    return {
-      statusCode: 301,
-      headers: {
-        location: `https://wondrous-malasada-3ffc32.netlify.app/post/${event.queryStringParameters.pid}.html`
-      }
-    }
-  }
+  return event.headers.referer;
 }
 
-// https://wondrous-malasada-3ffc32.netlify.app/.netlify/functions/post/1001/
-// https://wondrous-malasada-3ffc32.netlify.app/.netlify/functions/redirects?pid=20955
+// exports.handler = async event => {
+//   if (event.headers.referer && event.headers.referer.includes('facebook')) {
+//     return {
+//       statusCode: 301,
+//       headers: {
+//         location: `https://www.hiptoro.com/?page_id=${event.queryStringParameters.pid}` 
+//       }
+//     }
+//   } else {
+//     return {
+//       statusCode: 301,
+//       headers: {
+//         location: `https://wondrous-malasada-3ffc32.netlify.app/post/${event.queryStringParameters.pid}.html`
+//       }
+//     }
+//   }
+// }
+
+// // https://wondrous-malasada-3ffc32.netlify.app/.netlify/functions/post/1001/
+// // https://wondrous-malasada-3ffc32.netlify.app/.netlify/functions/redirects?pid=20955
