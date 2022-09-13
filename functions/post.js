b8f33f4c2fa8d1ec310457df1b03dc69c69b62bd
muzamilabbas <muzamilabbas2010@yahoo.com>
2022-09-13 21:25:50 +0500

    Update post.js

--- functions/post.js
+++ functions/post.js
@@ -1,10 +1,20 @@
-exports.handler = async () => {
-  return {
-    statusCode: 301,
-    headers: {
-      location: 'https://www.hiptoro.com/'
+exports.handler = async event => {
+  if (event.headers.referer && event.headers.referer.includes('facebook')) {
+    return {
+      statusCode: 301,
+      headers: {
+        location: `https://www.hiptoro.com/?page_id=${event.queryStringParameters.pid}` 
+      }
+    }
+  } else {
+    return {
+      statusCode: 301,
+      headers: {
+        location: `https://wondrous-malasada-3ffc32.netlify.app/post/${event.queryStringParameters.pid}.html`
+      }
     }
   }
 }
 
 // https://wondrous-malasada-3ffc32.netlify.app/.netlify/functions/post/1001/
+// https://wondrous-malasada-3ffc32.netlify.app/.netlify/functions/redirects?pid=20955
